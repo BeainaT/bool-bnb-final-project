710709f92896b30d55897abe1930173776d9a7b6
LeleGuidi <emanuele.guidi98@outlook.it>
2022-08-29 17:09:55 +0200

    Add default image for houses seeder

--- resources/views/user/houses/show.blade.php
+++ resources/views/user/houses/show.blade.php
@@ -9,10 +9,15 @@
                     <h2>Il tuo appartamento nel dettaglio</h2>
                 </div>
                 <div class="card-body">
+                    
                     <ul>
                         {{-- @foreach ($houses as $house) --}}
                             <li>
-                                <img style="width:50px;" src="{{asset('storage/'.$house->image)}}" alt="">
+                                @if ($house->image == "default")
+                                    <img style="width:50px;" src="{{asset('img/houses/default_house.jpeg')}}" alt="">
+                                @else
+                                    <img style="width:50px;" src="{{asset('storage/'.$house->image)}}" alt="">
+                                @endif
                                 {{$house->name}}
                                 {{$house->address}}
                             </li>                            
