4ef77226b70ca2d160fb7fea9bf82aa870db0616
BeainaT <tania.df@hotmail.it>
2022-09-01 09:40:11 +0200

    fixed sources in webpack

--- public/js/front.js
+++ public/js/front.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -13633,7 +13633,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ 1:
+/***/ 2:
 /*!*************************************!*\
   !*** multi ./resources/js/front.js ***!
   \*************************************/
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js(['resources/js/app.js', 'resources/js/scripts/pswConfirm.js'], 'public/js')
+mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/scripts/pswConfirm.js', 'public/js')
     .js('resources/js/front.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/guest/front.scss', 'public/css');
