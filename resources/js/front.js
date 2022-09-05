e1001b1b7bb813f08f76971869c25c8423175988
LeleGuidi <emanuele.guidi98@outlook.it>
2022-09-05 16:50:32 +0200

    fixed vue routing

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,11 +1,16 @@
 window.Vue = require('vue');
-window.axios = require('axios');
-// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-import Vue from 'vue';
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// aggiungiamo l'import del file router.js
+import router from "./router";
+	
 import App from './views/App.vue';
 
 const app = new Vue({
     el: '#app',
-    render: h=> h(App)
-})
+    render: h => h(App),
+    router
+});
---
+++ resources/js/pages/Page404.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="page404">
+        <h1>Pagina non trovata :(</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+
+</style>
