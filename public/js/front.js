98e123570599b18c6a00ec69e99eecb042a627c6
Ettore Barcella <ettore.barcella@gmail.com>
2022-09-07 12:19:29 +0200

    added first graphic

--- public/js/front.js
+++ public/js/front.js
@@ -19172,7 +19172,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\tania\Desktop\BOOLEAN\bool-bnb-final-project\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\esercizi_php\bool-bnb-final-project\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
